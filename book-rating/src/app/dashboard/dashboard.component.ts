c809648b60c703e36c4f9757e88c06621e3759d1
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-04-25 15:55:06 +0200

    dashboard: showing books as strings

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cs-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  books: string[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.books = ['Angular', 'Angular JS 1.x'];
+  }
+}
