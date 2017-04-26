eda61c97977e97af9afe7d0a14a82f23d677bb08
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-04-26 11:17:35 +0200

    my first unit test

--- book-rating/src/app/shared/book.spec.ts
+++ book-rating/src/app/shared/book.spec.ts
@@ -1,6 +1,38 @@
-import {Book} from './book';
+import { Book } from './book';
+
 
 describe('Book', () => {
+
+  let book: Book;
+
+  beforeEach(() => {
+    book = new Book('', '', '');
+  });
+
+  it('should increase the rating', () => {
+    book.rateUp();
+    expect(book.rating).toBe(1);
+  });
+
+  it('should descrease the rating', () => {
+    book.rating = 5;
+    book.rateDown();
+    expect(book.rating).toBe(4);
+  });
+
+  it('should not be allowed to have a rating greater than 5', () => {
+    book.rating = 5;
+    book.rateUp();
+    expect(book.rating).toBe(5);
+  });
+
+  it('should not be allowed to have a rating smaller than 0', () => {
+    book.rating = 0;
+    book.rateDown();
+    expect(book.rating).toBe(0);
+  });
+
+
   it('should create an instance', () => {
     expect(new Book('', '', '')).toBeTruthy();
   });
