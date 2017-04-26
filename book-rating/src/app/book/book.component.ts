ccafd11445490e96494e21f7e995f1e102e0aa49
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-04-26 10:06:49 +0200

    cleanup

--- book-rating/src/app/book/book.component.ts
+++ book-rating/src/app/book/book.component.ts
@@ -5,8 +5,7 @@ import { Book } from '../shared/book';
 @Component({
   selector: 'cs-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./book.component.css']
 })
 export class BookComponent {
   @Input() book: Book;
