2874fc20f21af54ec9a92413bbc9624fe4854fe1
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-04-27 10:46:11 +0200

    routing!

--- book-rating/src/app/app-routing.module.ts
+++ book-rating/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
+import { BookDetailsComponent } from './book-details/book-details.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'book/:isbn', component: BookDetailsComponent }
 ];
 
 @NgModule({
